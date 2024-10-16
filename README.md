c0f807f2e03eb304cd2b40b405e53cf7dbad38e1
riccardopoiani <riccardo.poiani@polimi.it>
2024-10-16 15:35:05 +0200

    Codebase

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
-# irl-sub-opt
+# Code for the paper "Sub-optimal Experts mitigate Ambiguity in Inverse Reinforcement Learning" (NeurIPS 2024)
+This repository contains the code to reproduce the experiments in the paper "Sub-optimal Experts mitigate Ambiguity in Inverse Reinforcement Learning" (Poiani et al., NeurIPS 2024).
+
+We relied on `python 3.8` for the implementation. The file `requirements.txt` contains the necessary dependency.
+
+To reproduce the experiment with perfect knowledge on the MDP, run the command `python correct_exp.py`.
+To reproduce the experiment without knowledge of the mdp, run the command `python emp_exp_ub.py`. 
+Within this last file it is also possible to modify the value of epsilon, delta, and the number of runs.
---
+++ correct_exp.py
@@ -0,0 +1,72 @@
+import mdptoolbox.example
+import numpy as np
+import scipy.sparse as _sp
+import matplotlib.pyplot as plt
+
+import mdp
+
+
+def compute_discounted_visitation_matrix(T, pol, num_s, num_a, discount):
+    Ppolicy = np.empty((num_s, num_s))
+    for aa in range(num_a):  # avoid looping over S
+        # the rows that use action a.
+        ind = (pol == aa).nonzero()[0]
+        # if no rows use action a, then no need to assign this
+        if ind.size > 0:
+            try:
+                Ppolicy[ind, :] = T[aa][ind, :]
+            except ValueError:
+                Ppolicy[ind, :] = T[aa][ind, :].todense()
+    return np.linalg.solve((_sp.eye(num_s, num_s) - discount * Ppolicy), np.eye(num_s))
+
+
+g = 0.9
+n_states = 10
+n_actions = 2
+
+P, R = mdptoolbox.example.forest(S=10)
+R /= R.max()
+pi = mdp.PolicyIteration(P, R, g)
+
+# Build
+policy_list, value_list = pi.run()
+policy_list = policy_list[:-1]
+value_list = value_list[:-1]
+
+expert_policy = policy_list[-1]
+
+# Compute xi bounds
+xi_list = []
+for v in value_list:
+    xi_list.append(np.abs(v - value_list[-1]).max())
+n_experts = len(xi_list) - 1
+
+# Compute for each of the sub-optimal agents the discounted occupancy matrix
+d_pi_list = []
+for pol in policy_list[:-1]:
+    d_pi_list.append(compute_discounted_visitation_matrix(P, pol, n_states, n_actions, g))
+
+
+# Compute maximum value of zeta
+max_bounds = [[] for _ in range(n_states)]
+for s in range(n_states):
+    for a in range(n_actions):
+        temp = []
+        for exp in range(n_experts):
+            if policy_list[exp][s] == a:
+                for s_new in range(n_states):
+                    if d_pi_list[exp][s_new,s] > 0:
+                       temp.append(xi_list[exp] / d_pi_list[exp][s_new, s])
+        if len(temp) == 0:
+            max_bounds[s].append(1 / (1-g))
+        else:
+            max_bounds[s].append(min(temp))
+
+max_bounds = np.array(max_bounds)
+max_bounds = max_bounds.flatten()
+ax = plt.figure().gca()
+ax.set_xticks([i for i in range(n_states * n_actions)])
+plt.ylabel(r"Upper bound on $\zeta(s,a)$")
+plt.xlabel("(s,a) index")
+plt.plot(max_bounds, marker='o', linestyle='None')
+plt.savefig("figures/exact_ub.png")
